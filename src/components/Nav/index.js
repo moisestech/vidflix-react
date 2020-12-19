9f850e06b83b1eed415527ec9f5ec1e667779dbc
Moises Sanabria <sesmoi.data@gmail.com>
2020-12-19 15:31:22 -0500

    init Nav func comp

---
+++ src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Nav () {
+  return (
+    <div className='nav'>
+      Nav
+    </div>
+  )
+}
+
+Nav.propTypes = {
+
+}
