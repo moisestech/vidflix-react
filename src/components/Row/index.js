72e16fa1d152a35ae6b6698fef98a065ee5749d3
Moises Sanabria <sesmoi.data@gmail.com>
2020-12-19 15:31:36 -0500

    init Row func comp

---
+++ src/components/Row/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Row () {
+  return (
+    <div className='row'>
+      Row
+    </div>
+  )
+}
+
+Row.propTypes = {
+
+}
