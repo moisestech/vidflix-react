a727ec8c9a2079494bd32e0c0177ff179d925d14
Moises Sanabria <sesmoi.data@gmail.com>
2020-12-19 15:31:05 -0500

    init Banner func comp

---
+++ src/components/Banner/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Banner () {
+  return (
+    <div className='banner'>
+      Banner
+    </div>
+  )
+}
+
+Banner.propTypes = {
+
+}
